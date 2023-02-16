3d9705c5824f6f159f5f27546d32fbbf7cef2856
alyssa-cheng <alyssacheng@g.ucla.edu>
2023-02-16 01:38:26 -0800

    Website Base 2
    Working base, but have not implemented the voter data in the website.

--- app.py
+++ app.py
@@ -1,22 +1,60 @@
 from flask import Flask
 from flask import render_template, abort, redirect, url_for
 from flask import request, g
+import pandas as pd
 import sqlite3
+import voting_systems
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def render_main():
-    return render_template("main.html")
+    if request.method == "GET":
+        # g.table = voting_systems.make_table() #change this later
+        return render_template("main.html")
+    else:
+        url = request.form["system"]
+        return redirect(url_for(url))
 
-@app.route("/plurality/")
+@app.route("/plurality/", methods = ["GET", "POST"])
 def render_plurality():
-    return render_template("plurality.html")
+    if request.method == "GET":
+        return render_template("plurality.html")
+    else:
+        url = request.form["system"]
+        # results = voting_systems.plurality()
+        return redirect(url_for(url))
 
-@app.route("/bordacount/")
+def plural_df():
+    pluralityList = voting_systems.plurality()
+    pluralityDF = pd.DataFrame(pluralityList, columns = ['candidate', 'number of votes'])
+    # return render_template('plurality.html',  tables=[pluralityDF.to_html(classes='data')], titles=pluralityDF.columns.values)
+
+
+@app.route("/bordacount/", methods = ["GET", "POST"])
 def render_borda():
-    return render_template("bordacount.html")
+    if request.method == "GET":
+        return render_template("bordacount.html")
+    else:
+        try:
+            url = request.form["system"]
+            results = voting_systems.borda()
+            return redirect(url_for(url))
+        except:
+            return render_template("bordacount.html")
 
-@app.route("/rankchoice/")
+@app.route("/rankchoice/", methods = ["GET", "POST"])
 def render_rcv():
-    return render_template("rankchoice.html")
+    if request.method == "GET":
+        return render_template("rankchoice.html")
+    else:
+        url = request.form["system"]
+        return redirect(url_for(url))
+
+@app.route("/toptwo/", methods = ["GET", "POST"])
+def render_toptwo():
+    if request.method == "GET":
+        return render_template("toptwo.html")
+    else:
+        url = request.form["system"]
+        return redirect(url_for(url))
--- voting_systems.py
+++ voting_systems.py
@@ -1,16 +1,87 @@
 import sqlite3
 import pandas as pd
 
+results = ['Tulsi Gabbard',
+ 'Elizabeth Warren',
+ 'Pete Buttigieg',
+ 'Michael R. Bloomberg',
+ 'Tom Steyer',
+ 'Joseph R. Biden',
+ 'Bernie Sanders',
+ 'Amy Klobuchar']
+
 def make_table():
     with sqlite3.connect("voter_data.db") as conn:
-        cursor = conn.cursor()
+        df = pd.read_csv("~/Documents/GitHub/course-project-16b/alaska_presidentialelection.csv")
+        df = df[["rank1","rank2","rank3","rank4","rank5"]]
+        df.to_sql("votes",conn,index=False,if_exists="replace")
         
 
 def plurality():
-    pass
+    with sqlite3.connect("voter_data.db") as conn:
+        cursor = conn.cursor()
+        cmd = """
+        SELECT rank1, COUNT(rank1)
+        FROM votes
+        GROUP BY rank1
+        ORDER BY COUNT(rank1) DESC
+        """
+        cursor.execute(cmd)
+        return cursor.fetchall()
 
-def borda():
-    pass
+def borda(point_dict={1:1,2:2,3:3,4:4,5:5}):
+    #ask if there's a more efficient way to do this
+    
+    count = {}
+    with sqlite3.connect("voter_data.db") as conn:
+        cursor = conn.cursor()
+        for result in results:
+            for col in [1,2,3,4,5]:
+                cmd = f"SELECT {point_dict[col]}*COUNT(rank{col}) FROM votes WHERE (rank{col}=='{result}')"
+                cursor.execute(cmd)
+                count[result] = count.get(result,0) + cursor.fetchone()[0]
+    return [(i,j) for i,j in count.items()]
 
-def IRV():
-    pass
+def TopTwo():
+    plural = plurality()
+    top_two = [plural[0][0],plural[1][0]]
+    with sqlite3.connect("voter_data.db") as conn:
+        cursor = conn.cursor()
+        #hard-coded because I have no idea how else to do this
+        cmd = f"""
+        SELECT COUNT(*) FROM votes
+        WHERE (rank1 == '{top_two[0]}' AND rank2 == '{top_two[1]}')
+        OR (rank1 == '{top_two[0]}' AND rank3 == '{top_two[1]}')
+        OR (rank1 == '{top_two[0]}' AND rank4 == '{top_two[1]}')
+        OR (rank1 == '{top_two[0]}' AND rank5 == '{top_two[1]}')
+        OR (rank2 == '{top_two[0]}' AND rank3 == '{top_two[1]}')
+        OR (rank2 == '{top_two[0]}' AND rank4 == '{top_two[1]}')
+        OR (rank2 == '{top_two[0]}' AND rank5 == '{top_two[1]}')
+        OR (rank3 == '{top_two[0]}' AND rank4 == '{top_two[1]}')
+        OR (rank3 == '{top_two[0]}' AND rank5 == '{top_two[1]}')
+        OR (rank4 == '{top_two[0]}' AND rank5 == '{top_two[1]}')
+        OR (rank5 == '{top_two[0]}' AND rank1 != '{top_two[1]}' AND rank2 != '{top_two[1]}'
+        AND rank3 != '{top_two[1]}' AND rank4 != '{top_two[1]}')
+        """
+        cursor.execute(cmd)
+        count1 = cursor.fetchone()[0]
+        
+        cmd = f"""
+        SELECT COUNT(*) FROM votes
+        WHERE (rank1 == '{top_two[1]}' AND rank2 == '{top_two[0]}')
+        OR (rank1 == '{top_two[1]}' AND rank3 == '{top_two[0]}')
+        OR (rank1 == '{top_two[1]}' AND rank4 == '{top_two[0]}')
+        OR (rank1 == '{top_two[1]}' AND rank5 == '{top_two[0]}')
+        OR (rank2 == '{top_two[1]}' AND rank3 == '{top_two[0]}')
+        OR (rank2 == '{top_two[1]}' AND rank4 == '{top_two[0]}')
+        OR (rank2 == '{top_two[1]}' AND rank5 == '{top_two[0]}')
+        OR (rank3 == '{top_two[1]}' AND rank4 == '{top_two[0]}')
+        OR (rank3 == '{top_two[1]}' AND rank5 == '{top_two[0]}')
+        OR (rank4 == '{top_two[1]}' AND rank5 == '{top_two[0]}')
+        OR (rank5 == '{top_two[1]}' AND rank1 != '{top_two[0]}' AND rank2 != '{top_two[0]}'
+        AND rank3 != '{top_two[0]}' AND rank4 != '{top_two[0]}')
+        """
+        cursor.execute(cmd)
+        count2 = cursor.fetchone()[0]
+        
+    return list(zip(top_two,[count1,count2]))
