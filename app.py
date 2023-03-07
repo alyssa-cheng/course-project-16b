97703540c6ae1a009db49031f595e7f842c5a79f
alyssa-cheng <alyssacheng@g.ucla.edu>
2023-03-06 21:01:36 -0800

    main, intro, start pages
    made buttons for main and intro pages
    
    changed plurality to table instead of graph

--- app.py
+++ app.py
@@ -16,29 +16,36 @@ def get_plurality_df():
     pluralityDF = pd.DataFrame(pluralityList, columns = ['candidate', 'number of votes'])
     return pluralityDF
 
-def plurality_graph():
-    df = get_plurality_df()
-    fig = px.bar(data_frame = df, 
-                 x = 'candidate', 
-                 y = 'number of votes',
-                 hover_name = 'candidate',
-                 width = 700,
-                 height = 400)
+# def plurality_graph():
+#     df = get_plurality_df()
+#     fig = px.bar(data_frame = df, 
+#                  x = 'candidate', 
+#                  y = 'number of votes',
+#                  hover_name = 'candidate',
+#                  width = 700,
+#                  height = 400)
 
-    fig.update_layout(title = "Plurality Candidate Votes", xaxis_title = 'Name of Candidate', yaxis_title = 'Number of Votes')
+#     fig.update_layout(title = "Plurality Candidate Votes", xaxis_title = 'Name of Candidate', yaxis_title = 'Number of Votes')
 
-    return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+#     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 def get_borda_df(point_dict = {1:1, 2:2, 3:3, 4:4, 5:5}):
     bordaList = voting_systems.borda("votes", point_dict)
     bordaDF = pd.DataFrame(bordaList, columns = ['candidate', 'number of votes'])
     return bordaDF
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/")
 def render_main():
+    return render_template("main.html")
+    
+@app.route("/intro/")
+def render_intro():
+    return render_template("intro.html")
+
+@app.route("/start/", methods = ["GET", "POST"])
+def render_start():
     if request.method == "GET":
-        # g.table = voting_systems.make_table() #change this later
-        return render_template("main.html")
+        return render_template("start.html")
     else:
         url = request.form["system"]
         return redirect(url_for(url))
@@ -46,8 +53,9 @@ def render_main():
 @app.route("/plurality/", methods = ["GET", "POST"])
 def render_plurality():
     if request.method == "GET":
-        graphJSON = plurality_graph()
-        return render_template("plurality.html", graphJSON = graphJSON)
+        pluralityDF = get_plurality_df()
+        pluralityDF = pluralityDF.to_html(index = False)
+        return render_template("plurality.html", pluralityDF = pluralityDF)
     else:
         url = request.form["system"]
         return redirect(url_for(url))
@@ -56,12 +64,12 @@ def render_plurality():
 def render_borda():
     if request.method == "GET":
         bordaDF_og = get_borda_df()
-        bordaDF_og = bordaDF_og.to_html()
+        bordaDF_og = bordaDF_og.to_html(index = False)
         return render_template("bordacount.html", bordaDF_og = bordaDF_og)
     else:
         if request.form["submit"] == "Submit Rank Values":
             bordaDF_og = get_borda_df()
-            bordaDF_og = bordaDF_og.to_html()
+            bordaDF_og = bordaDF_og.to_html(index = False)
             
             rank1 = request.form['rank1']
             rank2 = request.form['rank2']
